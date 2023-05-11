5f1d4a2577cc6ee94adb445fdbb8021b4454db4f
Akshay Badola <akshay.badola.cs@gmail.com>
2023-05-11 11:35:13 +0530

    Added demo to experiments.py

--- experiments.py
+++ experiments.py
@@ -1,11 +1,64 @@
 import hydra
 import torch
+from functools import partial
 
 import cramming
 from cramming import utils
 
 
 hydra.initialize(config_path="cramming/config")
+
+
+class CollectOutputs:
+    def __init__(self, model):
+        self.model = model
+        self.model = self.model.eval()
+        self._module_names = [x[0] for x in model.named_modules()]
+        self._results = {}
+        self._handles = {}
+
+    def clear_all_hooks(self):
+        for v in self._handles.values():
+            v.remove()
+        self._handles = {}
+
+    def remove_hooks(self, module_names=None):
+        if not module_names:
+            module_names = self._module_names
+        for k in module_names:
+            v = self._handles.pop(k)
+            v.remove()
+
+    def debug_hook(self, module_name, *x):
+        import ipdb; ipdb.set_trace()
+
+    def add_debug_hook(self, module_names):
+        if self._handles:
+            for v in self._handles.values():
+                v.remove()
+            self._handles = {}
+        self._handles = {module_name: self.model.get_submodule(module_name).
+                         register_forward_hook(partial(self.debug_hook, module_name))
+                         for module_name in module_names}
+
+    def collect_hook(self, module_name, *x):
+        if module_name not in self._results:
+            self._results[module_name] = []
+        self._results[module_name].append((x[0], x[1].detach().cpu(), x[2].detach().cpu()))
+
+    def add_collect_hook(self, module_names):
+        if self._handles:
+            for v in self._handles.values():
+                v.remove()
+            self._handles = {}
+        self._handles = {module_name: self.model.get_submodule(module_name).
+                         register_forward_hook(partial(self.collect_hook, module_name))
+                         for module_name in module_names}
+
+    def get_result_for(self, module_name, indx=-1):
+        mod, inputs, outputs = self._results[module_name][indx]
+        return {"module": mod, "input": inputs, "output": outputs}
+
 
 
 def load_fixing_names(model, state_dict):
@@ -30,3 +83,14 @@ def get_model(arch, checkpoint_path, tokenizer_path):
     elif isinstance(state, dict):
         load_fixing_names(model, state["model_state"])
     return model, tokenizer
+
+
+def demo(arch, checkpoint_path, tokenizer_path):
+    model, tokenizer = get_model(arch, checkpoint_path, tokenizer_path)
+    collect = CollectOutputs(model)
+    collect.add_collect_hook(model, ['encoder.layers.0.attn'])
+    text = "There is a light that"
+    encoded_input = tokenizer(text, return_tensors='pt')
+    with torch.no_grad():
+        output = model(**encoded_input)
+    print(collect.get_result_for('encoder.layers.0.attn'))
